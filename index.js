b186b4b2df8ec9d5024fee23b30bd63a13975aec
John Fraboni <johnfraboni@hotmail.com>
2016-07-08 04:30:00 +0000

    * update readme

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const customers = require("./data/customers.json");
 
 
 /**
- * 1. Import your lodown module using the require() method
+ * 1. Import your lodown module using the require() method, 
+ *    using the string 'lodown-<my-username>', or whatever 
+ *    name with which you published your npm lodown project.
+ * 
  * 2. Solve all problems as outlined in the README.
  */
  
